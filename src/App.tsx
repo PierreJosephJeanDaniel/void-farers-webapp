a225ed18cd9a123c1f602ada08385ed096590690
PierreJosephJeanDaniel <45936836+PierreJosephJeanDaniel@users.noreply.github.com>
2024-11-21 10:21:03 +0100

    Added Supabase Auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,46 @@ import CharacterSheet from "./Components/CharacterSheet/CharacterSheet";
 import Starfield from "./Components/Starfield/Starfield";
 import store from "./Store";
 import Popup from "./Components/Popup/Popup";
+import { useEffect, useState } from "react";
+import { Session, createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+// Variables have to be prefixed with VITE_ to be accessible in the client side (https://vite.dev/guide/env-and-mode.html#env-files)
+
+const ApiKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SupabaseUlr: string = import.meta.env.VITE_SUPABASE_PROJECT_URL;
+
+const supabase = createClient(SupabaseUlr, ApiKey);
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Starfield />
-      <CharacterSheet name={"Bilbo Baggins"} />
-      <Popup />
-    </Provider>
-  );
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return (
+      <Provider store={store}>
+        <Starfield />
+        <CharacterSheet name={"Bilbo Baggins"} />
+        <Popup />
+      </Provider>
+    );
+  }
 }
 
 export default App;
