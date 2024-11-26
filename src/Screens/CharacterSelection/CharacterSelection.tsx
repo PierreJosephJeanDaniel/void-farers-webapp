bd77303dae9e8ce9dbe0165fc95a5ce8845bc7c7
PierreJosephJeanDaniel <45936836+PierreJosephJeanDaniel@users.noreply.github.com>
2024-11-26 20:29:08 +0100

    Added formatting for character selection and return navigation arrow

--- src/Screens/CharacterSelection/CharacterSelection.tsx
+++ src/Screens/CharacterSelection/CharacterSelection.tsx
@@ -2,11 +2,24 @@ import React, { useEffect, useState } from "react";
 import { RootState } from "../../Store";
 import { useSelector } from "react-redux";
 import { supabaseManager } from "../../Managers/SupabaseManager/SupabaseManager";
+import "./CharacterSelection.css";
+import Airlock from "../../Components/AirLock/AirLock";
+import ReturnArrow from "../../Components/ReturnArrow/ReturnArrow";
+import { useNavigate } from "react-router-dom";
 
 const CharacterSelection: React.FC = () => {
   const [characters, setCharacters] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const userId = useSelector((state: RootState) => state.auth.userId);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await supabaseManager.LogOut();
+  };
+
+  const handleGoBack = () => {
+    navigate("/landing");
+  };
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -24,17 +37,23 @@ const CharacterSelection: React.FC = () => {
   }, [userId]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="loading-fidget">Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>C</h1>
-      <ul>
+    <div className="main-container">
+      <header className="landing-header">
+        <ReturnArrow onClick={handleGoBack} />
+        <Airlock onClick={handleLogout} />
+      </header>
+      <div className="title">Available characters</div>
+      <div className="body">
         {characters.map((name, index) => (
-          <li key={index}>{name}</li>
+          <div className="character" key={index}>
+            {name}
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
